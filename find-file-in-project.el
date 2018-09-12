a3d89bc16432bd44d4a4ebfd01dab57a88025327
Chen Bin <chenbin.sh@gmail.com>
2018-09-12 22:18:57 +1000

    documenation

--- find-file-in-project.el
+++ find-file-in-project.el
@@ -77,15 +77,15 @@
 ;; Sample .dir-locals.el,
 ;;
 ;; ((nil . ((ffip-project-root . "~/projs/PROJECT_DIR")
-;;          ;; ingore files bigger than 64k and directory "dist/"
+;;          ;; ignore files bigger than 64k and directory "dist/" when searching
 ;;          (ffip-find-options . "-not -size +64k -not -iwholename '*/dist/*'")
 ;;          ;; only search files with following extensions
 ;;          (ffip-patterns . ("*.html" "*.js" "*.css" "*.java" "*.xml" "*.js"))
 ;;          (eval . (progn
 ;;                    (require 'find-file-in-project)
-;;                    ;; ingore directory ".tox/"
+;;                    ;; ignore directory ".tox/" when searching
 ;;                    (setq ffip-prune-patterns `("*/.tox/*" ,@ffip-prune-patterns))
-;;                    ;; Do NOT ignore directory "bin/"
+;;                    ;; Do NOT ignore directory "bin/" when searching
 ;;                    (setq ffip-prune-patterns `(delete "*/bin/*" ,@ffip-prune-patterns))))
 ;;          )))
 ;;
